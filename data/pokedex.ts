09a285a7def2c2e3b241fe5d554b7ef8cdb0ef36
Osome32 <isosome@protonmail.com>
2026-03-19 22:39:01 +0530

    Test

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -12,6 +12,18 @@ export const Pokedex: import('../sim/dex-species').SpeciesDataTable = {
 		evos: ["Ivysaur"],
 		eggGroups: ["Monster", "Grass"],
 	},
+	bsas: {
+		num: -101,
+		name: "bsas",
+		types: ["Grass", "Poison"],
+		genderRatio: { M: 0.875, F: 0.125 },
+		baseStats: { hp: 45, atk: 49, def: 49, spa: 65, spd: 65, spe: 45 },
+		abilities: { 0: "Overgrow", H: "Chlorophyll" },
+		heightm: 0.7,
+		weightkg: 6.9,
+		color: "Green",
+		eggGroups: ["Monster", "Grass"],
+	},
 	ivysaur: {
 		num: 2,
 		name: "Ivysaur",
